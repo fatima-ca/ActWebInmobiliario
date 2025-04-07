03b3f1ae2e48011c6892f31805df56968ba45db7
fatima-ca <fatimacastilloag@gmail.com>
2025-04-07 07:59:36 -0600

    agregue types->db->house.ts

---
+++ src/types/db/house.ts
@@ -0,0 +1,18 @@
+/*Notas:
+Puede ser una clase o interface
+class:
+interface: para ponerle un vlaor inicial, sin logica
+*/
+
+//interface en este caso: estmos simulando que obtenemos datos de una base de datos
+export interface House {
+    precio: number,
+    ubicacion: string,
+    descripcion: string,
+    calificaicon:number,
+    tamanioconstruccion: number,
+    tamanioterreno: number,
+    duracion: number,
+    tipo: string
+}
+
